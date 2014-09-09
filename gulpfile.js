9d371464bc84cb7018a42ee9eae634adada560bb
David Arceneaux <david@davidthemachine.org>
2014-09-09 14:40:16 -0700

    farewell Makefile, hello gulpfile
    include all the necessary pieces to get gulp working

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var
+  gulp = require('gulp'),
+  cssBase64 = require('gulp-css-base64'),
+  minifycss = require('gulp-minify-css'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  watch = require('gulp-watch');
+
+gulp.task('clean', function(cb) {
+  del(['normalize.css','normalize.css.map'], cb);
+});
+
+gulp.task('styles', function() {
+  return gulp.src('normalize.scss')
+    .pipe(sass())
+    .pipe(cssBase64({
+      verbose: true,
+      baseDir: './',
+      extensionsAllowed: ['.gif','.png','.svg','.jpg']
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(minifycss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./'))
+//    .on('error', function(err) { console.log(err.message); });
+});
+
+gulp.task('default', ['watch','styles']);
+
+gulp.task('watch', function() {
+  gulp.watch(['normalize.scss','_normalize.scss','assets/_rem.scss'], ['styles']);
+});
+
